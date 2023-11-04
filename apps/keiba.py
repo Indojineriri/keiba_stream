e64e6268d7c62660547096ec06db86ffb550e7e1
shoheisato <satojuve@gmail.com>
2023-11-04 20:49:37 +0900

    for streamlit

---
+++ apps/keiba.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+
+sns.set()
+st.title('東京競馬2013～2023')
+def calc_binned_avg(feature, target, bins):
+    df_binned = df.copy()
+    df_binned['binned'] = pd.cut(df_binned[feature], bins=bins, include_lowest=True)
+    return df_binned.groupby('binned')[target].mean().reset_index()
+
+df=pd.read_pickle('apps\merged_tokyo_turf.pickle')
+setting_length = st.selectbox(
+    'コースの長さを選択してください:',
+    options=df['course_len'].unique(), # course_lenのユニークな値を選択肢として提供
+    format_func=lambda x: f'{int(x*100)}m' # 選択肢をメートル単位で表示
+)
+idx=df['course_len']==setting_length
+df=df[idx].copy()
+#縦軸、横軸の設定
+feature = st.selectbox(
+    '比較する特徴量を選択してください:',
+#    options=df.columns.drop('rank') # 'rank'を除外したすべての特徴量
+    options=['枠 番','斤量','Weight','Goal difference_5R','Bonus_5R','first_corner_5R','speed_1R']
+)
+# ビンの作成
+num_bins = st.number_input('分割数を入力してください', min_value=1, max_value=100, value=10)
+range_min, range_max = st.slider(
+    'グラフのレンジを選択してください',
+    min_value=float(df[feature].min()),
+    max_value=float(df[feature].max()),
+    value=(float(df[feature].min()), float(df[feature].max()))
+)
+
+# ビンの作成、ここではユーザーが指定した範囲を使用
+bins = np.linspace(start=range_min, stop=range_max, num=num_bins)
+
+# 平均順位の計算
+avg_rank = calc_binned_avg(feature, 'rank', bins)
+
+# プロット作成
+fig_BS = make_subplots(rows=1, cols=1, subplot_titles=[feature])
+
+# 棒グラフの追加
+fig_BS.add_trace(go.Bar(x=avg_rank['binned'].astype(str), y=avg_rank['rank']), row=1, col=1)
+
+# レイアウト調整
+fig_BS.update_layout(barmode='group')
+
+# Streamlitにグラフを表示
+st.plotly_chart(fig_BS)
