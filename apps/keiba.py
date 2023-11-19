077427a2e78d4d4eb727c9ddde4eb488b0d8c8bc
shoheisato <satojuve@gmail.com>
2023-11-19 23:10:42 +0900

    fix for keiba

--- apps/keiba.py
+++ apps/keiba.py
@@ -11,20 +11,31 @@ import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 from matplotlib.font_manager import FontProperties
 from matplotlib import font_manager
-
+import lightgbm as lgb
 #jp_font_path = font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
 #jp_font = font_manager.FontProperties(fname=[f for f in jp_font_path if 'IPAexGothic' in f][0])
 sns.set()
 fonts = font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
 print(any('IPAexGothic' in font for font in fonts))
-# タイトル設定
-
+#パラメータ系の設定
 plt.rcParams['font.family'] = 'IPAexGothic'  # 日本語フォントに設定
 conversion_dict = {
         '枠番':'Waku',  '斤量':'斤量',  '馬体重':'Weight',  '平均着差':'Goal difference_5R',  '平均賞金額':'Bonus_5R',
         '平均Last3F':'Last3F_5R', '平均位置取り':'first_corner_5R' ,'前走スピード指数':'speed_1R'
         }
-selection=['枠番','斤量','馬体重','平均着差','平均賞金額','平均Last3F','平均位置取り','前走スピード指数']
+selection=['枠番','斤量','平均着差','平均賞金額','平均Last3F','平均位置取り','前走スピード指数']
+default_selections = ['平均着差', '平均賞金額', '平均Last3F', '平均位置取り']
+params = {'objective': 'binary',
+'random_state': 100,
+'feature_pre_filter': False,
+'lambda_l1': 0.0,
+'lambda_l2': 0.0,
+'num_leaves': 31,
+'feature_fraction': 0.6,
+'bagging_fraction': 0.5111969317302304,
+'bagging_freq': 1,
+'min_child_samples': 20,
+'num_iterations': 1000,}
 
 page=st.sidebar.radio('ページを選択してください',['データ分析','データ可視化'])
 
@@ -93,15 +104,23 @@ if page=='データ分析':
     
 elif page=='データ可視化':
     st.title('京都9-11レース')
-    df=pd.read_pickle('apps/calin.pickle')
+    tab = st.radio("競馬場を選択してください:", ('東京', '京都'))
+    if tab == '東京':
+        df_learn  = pd.read_pickle('apps/merged_tokyo_turf.pickle')
+        df=pd.read_pickle('apps/calin.pickle')
+    elif tab == '京都':
+        df_learn= pd.read_pickle('apps/merged_kyoto_turf.pickle')
+        df=pd.read_pickle('apps/calin.pickle')
+    #レースIDの取得
     race_id_unique=df.index.unique()
     race_id = st.selectbox(
         '予想するレースを選択してください:',
-    #    options=df.columns.drop('rank') # 'rank'を除外したすべての特徴量
+    #options=df.columns.drop('rank') # 'rank'を除外したすべての特徴量
         options=race_id_unique
     )
     idx=df.index==race_id
     df_use=df[idx].copy()
+    st.subheader("データ可視化（2軸で傾向見れるよーん）")
     default_feature1 = selection.index('平均位置取り') if '平均位置取り' in selection else 0
     default_feature2 = selection.index('平均Last3F') if '平均Last3F' in selection else 0
     feature1 = st.selectbox('第1の特徴量を選択してください:', selection,index=default_feature1)
@@ -122,3 +141,25 @@ elif page=='データ可視化':
 
     st.pyplot(plt)
     
+    #学習の開始
+    st.subheader("機械学習予想（LightGBMでやってるよーん）")
+    #特徴量の選択
+    selected_features = st.multiselect('特徴量を選択してください:', selection, default=default_selections)
+    ml_features = [conversion_dict.get(feature, feature) for feature in selected_features]
+    #コース長さの設定
+    course_length=df_use['course_len'].mean()
+    idx=df_learn['course_len']==course_length
+    #学習用のXを設定
+    X=df_learn[idx]
+    X = X[ml_features]
+    #学習用のYを設定
+    y=df_learn[idx]
+    y=y['rank']
+    #LGBMでの学習の実施
+    lgb_clf = lgb.LGBMClassifier(**params)
+    lgb_clf.fit(X, y)
+    #学習させるべきデータ
+    probabilities = lgb_clf.predict_proba(df_use[ml_features])[:, 1]
+    pred=df_use[['馬 番','馬名']].copy()
+    pred['複勝確率%']=probabilities*100
+    st.dataframe(pred)
